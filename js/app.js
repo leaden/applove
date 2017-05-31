a4dc1d2594e97805887062f7d5bc12080a6dc9d1
maribel <maribel.diaz.j@gmail.com>
2017-05-31 16:37:29 -0400

    apploveII

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+
+ (function main(){
+ 	var boxes = array.from(document.getelementsByClassName("box-services"));
+
+ })();
+
+var modal = document.getelementsById("box-services-modal");
+
+boxes.forEach(function(box){
+	box.addEventListener("click", function(){
+    modal.classList.remove("hide")
+	});
+ });
+
+var close = document.querySelector("#box-services-modal .close");
+close.addEventListener("click", function(){
+   modal.classList.add("hide");
+
+});
+})();
